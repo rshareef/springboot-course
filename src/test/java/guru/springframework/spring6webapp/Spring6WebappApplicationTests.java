fd0d2cfee6267f7e2689569b5045c6bfaef2d5bd
rshareef <ruqayyah.shareef@outlook.com>
2024-08-10 20:24:15 +0100

    Progress after video #15 of SpringBoot course (JPA Relationships).

---
+++ src/test/java/guru/springframework/spring6webapp/Spring6WebappApplicationTests.java
@@ -0,0 +1,14 @@
+package guru.springframework.spring6webapp;
+// default JUnit test.
+// this test is to check if the spring context (aka the built file) initialised, has been configured correctly.
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
